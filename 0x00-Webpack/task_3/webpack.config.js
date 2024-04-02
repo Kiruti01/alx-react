2891cd4cc70530827cbdad40d3fa700a13633e31
Michael Kiruti <mikekirutic@gmail.com>
2024-04-02 12:32:01 +0300

    mode

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
     header: "./modules/header/header.js",
     body: "./modules/body/body.js",
